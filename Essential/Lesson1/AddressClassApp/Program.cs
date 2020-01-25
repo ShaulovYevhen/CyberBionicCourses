f4381e3bdf434ed6d37021a7033adeabeef0ec42
GeeJaaY <gejay91@gmail.com>
2020-01-25 14:03:36 +0200

    AddressApp

--- Essential/Lesson1/AddressClassApp/Program.cs
+++ Essential/Lesson1/AddressClassApp/Program.cs
@@ -2,10 +2,69 @@
 
 namespace AddressClassApp
 {
+    class Address
+    {
+        private string index;
+        public string Index
+        {
+            get { return index; }
+            set { index = value; }
+        }
+
+        private string country;
+        public string Country
+        {
+            get { return country; }
+            set { country = value; }
+        }
+
+        private string city;
+        public string City
+        {
+            get { return city; }
+            set { city = value; }
+        }
+
+        private string street;
+        public string Street
+        {
+            get { return street;}
+            set { street = value; }
+        }
+
+        private string house;
+        public string House
+        {
+            get { return house; }
+            set { house = value; }
+        }
+
+        private string apartment;
+        public string Apartment
+        {
+            get { return apartment; }
+            set { apartment = value; }
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
+            Address address = new Address();
+            address.Index = "55500/02";
+            address.Country = "USA";
+            address.City = "Albukerke";
+            address.Street = "Pushkinsa";
+            address.House = "13b";
+            address.Apartment = "23a";
+
+            Console.WriteLine(address.Country);
+            Console.WriteLine(address.City);
+            Console.WriteLine(address.Street);
+            Console.WriteLine(address.House);
+            Console.WriteLine(address.Apartment);
+            Console.WriteLine(address.Index);
+            Console.ReadKey();
         }
     }
 }
