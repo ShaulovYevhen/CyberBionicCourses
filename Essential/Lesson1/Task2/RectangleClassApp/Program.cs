97ab66f57a9f2e9b90b5bbfcea08992d1c8aacd5
GeeJaaY <gejay91@gmail.com>
2020-01-30 13:36:58 +0200

    RectangleApp

--- Essential/Lesson1/Task2/RectangleClassApp/Program.cs
+++ Essential/Lesson1/Task2/RectangleClassApp/Program.cs
@@ -1,11 +1,31 @@
 ﻿using System;
+using static System.Console;
 
 namespace RectangleClassApp
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static double Input()
         {
+            double number;
+            while (!double.TryParse(Console.ReadLine(), out number))
+            {
+                Console.WriteLine("Error!Enter number");
+            }
+            return number;
+        }
+        public static void Main(string[] args)
+        {
+            WriteLine("Введите превую сторону прямоугольника");
+            var side1 = Input();
+
+            WriteLine("Введите вторую сторону прямоугольника");
+            var side2 = Input();
+            Rectangle rectangle = new Rectangle(side1, side2);
+
+            WriteLine("Perimeter = {0}, Area= {1}", rectangle.Perimeter, rectangle.Area);
+
+            ReadKey();
         }
     }
 }
---
+++ Essential/Lesson1/Task2/RectangleClassApp/Rectangle.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace RectangleClassApp
+{
+    public class Rectangle
+    {
+        readonly double _side1;
+        readonly double _side2;
+
+        public Rectangle(double side1, double side2) 
+        {
+            this._side1 = side1;
+            this._side2 = side2;
+        }
+
+        public double AreaCalculator()
+        {
+            return _side1*_side2;
+        }
+
+        public double Area
+        {
+            get { return this.AreaCalculator(); }
+        }
+
+        double PerimeterCalculator() 
+        {
+            return (_side1 + _side2) * 2;
+        }
+
+        public double Perimeter
+        {
+            get{ return this.PerimeterCalculator(); }
+        }
+    }
+}
