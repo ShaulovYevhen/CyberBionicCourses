c371bc651fe9c8661427d66ff15eeca38288dd23
GeeJaaY <gejay91@gmail.com>
2020-01-30 13:53:30 +0200

    BookApp

---
+++ Essential/Lesson1/Task3/BooksApp/BookApp.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BooksApp
+{
+    class BookApp
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
