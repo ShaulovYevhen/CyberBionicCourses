a2666a613d9f6001415dd1075b37673b439b218d
GeeJaaY <gejay91@gmail.com>
2020-03-16 09:59:38 +0200

    ClassRoomApp

--- Essential/Lesson3/Task2/ClassRoomApp/Program.cs
+++ Essential/Lesson3/Task2/ClassRoomApp/Program.cs
@@ -1,9 +1,19 @@
-﻿namespace ClassRoomApp
+﻿using System;
+
+namespace ClassRoomApp
 {
     class Program
     {
         static void Main(string[] args)
         {
+            var p1 = new BadPupil();
+            var p2 = new ExcellentPupil();
+            var firstClass = new ClassRoom(p1, p2);
+            Console.WriteLine(firstClass.ClassRoomStudy());
+            Console.WriteLine(firstClass.ClassRoomRead());
+            Console.WriteLine(firstClass.ClassRoomWrite());
+            Console.WriteLine(firstClass.ClassRoomRelax());
+            Console.ReadKey();
         }
     }
 }
